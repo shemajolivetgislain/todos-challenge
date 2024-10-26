b2cbd43505a2ac6714cabd3cc64217868a777e51
Shema Jolivet Gislain <shemajolivet@gmail.com>
2024-10-26 08:30:00 +0200

    fixed test issue

--- src/app/api/apiFetch.tsx
+++ src/app/api/apiFetch.tsx
@@ -1,18 +1,18 @@
-import { LOCAL_API_URL } from "../../constants";
+// src/utils/httpRequest.ts
 import HttpRequest from "../../utils/httpRequest";
 
 export const fetchData = async () => {
-  return await HttpRequest.get(`${LOCAL_API_URL}/todos`);
+  return await HttpRequest.get("https://dummyjson.com/todos");
 };
 
 export const createTask = async (data: any) => {
-  return await HttpRequest.post(`${LOCAL_API_URL}/todos/add`, data);
+  return await HttpRequest.post("https://dummyjson.com/todos/add", data);
 };
 
 export const updateTask = async ({ id, data }: { id: number; data: any }) => {
-  return await HttpRequest.put(`${LOCAL_API_URL}/todos/${id}`, data);
+  return await HttpRequest.put(`https://dummyjson.com/todos/${id}`, data);
 };
 
 export const deleteTask = async (id: any) => {
-  return await HttpRequest.delete(`${LOCAL_API_URL}/todos/${id}`);
+  return await HttpRequest.delete(`https://dummyjson.com/todos/${id}`);
 };
