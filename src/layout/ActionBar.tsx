5144d3ac734dbd0ace89db27496845850e7766a6
Shema Jolivet Gislain <shemajolivet@gmail.com>
2024-10-27 14:03:11 +0200

    redesigned navbar arrangement

--- src/layout/ActionBar.tsx
+++ src/layout/ActionBar.tsx
@@ -16,11 +16,11 @@ import {
 import { FaAngleDown } from "react-icons/fa";
 
 const ActionBar: React.FC = () => {
-//   const { t } = useTranslation();
+  //   const { t } = useTranslation();
   const dispatch = useDispatch();
-//   const languageChange = useSelector(
-//     (state: any) => state.todosActions.languageChange
-//   );
+  //   const languageChange = useSelector(
+  //     (state: any) => state.todosActions.languageChange
+  //   );
   const { defaultLanguage, translationlanguage, isLanguapePopMenuOpen } =
     useSelector((state: any) => state.translation);
   const [darkMode, setDarkMode] = useState<boolean>(() => {
@@ -56,6 +56,25 @@ const ActionBar: React.FC = () => {
   return (
     <>
       <div className="flex gap-3 items-center">
+        <div className="flex flex-col gap-5 items-center space-y-3">
+          <div
+            className="flex items-center gap-4 relative cursor-pointer"
+            onClick={() => {
+              dispatch(setIsLanguapePopMenuOpen(!isLanguapePopMenuOpen));
+            }}
+          >
+            <img
+              src={currentLanguage?.icon}
+              alt="flag"
+              className="w-6 h-6 rounded-full"
+            />
+            <span className=" flex items-center gap-2 text-black dark:text-white">
+              <p className="text-sm">{currentLanguage?.name}</p>
+              <FaAngleDown />
+            </span>
+          </div>
+          {isLanguapePopMenuOpen && <LanguageBar />}
+        </div>
         <ActionButton onClick={toggleThemeMode}>
           {darkMode ? (
             <CiLight
@@ -78,37 +97,13 @@ const ActionBar: React.FC = () => {
             <span className="absolute top-1 right-2 grid min-h-[6px] min-w-[6px] translate-x-1/2 -translate-y-1/2 place-items-center rounded-full bg-red-600 py-1 px-1 text-xs text-white"></span>
           </div>
         </ActionButton>
-        <div className="flex flex-col gap-5 items-center space-y-3">
-          <div
-            className="flex items-center gap-4 relative cursor-pointer"
-            onClick={() => {
-              dispatch(setIsLanguapePopMenuOpen(!isLanguapePopMenuOpen));
-            }}
-          >
-            <img
-              src={currentLanguage?.icon}
-              alt="flag"
-              className="w-7 h-7 rounded-full"
-            />
-            <span className=" flex items-center gap-2 text-black dark:text-white">
-              <p>{currentLanguage?.name}</p>
-              <FaAngleDown />
-            </span>
-          </div>
-          {isLanguapePopMenuOpen && <LanguageBar />}
-        </div>
-        {/* <ActionButton className="!relative !" onClick={handleLanguageChange}>
-          <p className="text-sm text-whiteTheme-primaryColor font-semibold dark:text-darkTheme-textColor">
-            {t("languageType")}
-          </p>
-        </ActionButton> */}
+
         <img
           src={picTwo}
           alt=""
           className="w-11 h-11 rounded-full object-fit"
         />
       </div>
-      {/* {languageChange && <LanguageAction />} */}
     </>
   );
 };
